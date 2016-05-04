4baf0acb57ab7340ed1f40556580e61b3e2c18ec
Daniel Janke <danijank@uni-koblenz.de>
2016-05-04 19:13:42 +0200

    added libraries

---
+++ src/de/unikoblenz/west/cep/Cep.java
@@ -0,0 +1,17 @@
+package de.unikoblenz.west.cep;
+
+/**
+ * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
+ *
+ */
+public class Cep {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}

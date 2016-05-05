a6ffe2d9d91b9bc2e0f28c303854a193ae3f663e
Daniel Janke <danijank@uni-koblenz.de>
2016-05-05 22:09:48 +0200

    continued implementation of loading time processing

---
+++ src/de/unikoblenz/west/cep/measurementProcessor/listeners/LoadGraphListener.java
@@ -0,0 +1,96 @@
+package de.unikoblenz.west.cep.measurementProcessor.listeners;
+
+import de.uni_koblenz.west.koral.common.measurement.MeasurementType;
+import de.uni_koblenz.west.koral.master.graph_cover_creator.CoverStrategyType;
+import de.unikoblenz.west.cep.measurementProcessor.MeasurmentListener;
+import de.unikoblenz.west.cep.measurementProcessor.utils.Utilities;
+
+/**
+ * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
+ *
+ */
+public abstract class LoadGraphListener implements MeasurmentListener {
+
+  private CoverStrategyType graphCoverStrategy;
+
+  private int nHopReplication;
+
+  @Override
+  public void processMeasurement(String... measurements) {
+    MeasurementType measurementType = Utilities.getMeasurementType(measurements);
+    if (measurementType != null) {
+      switch (measurementType) {
+        case LOAD_GRAPH_START:
+          graphCoverStrategy = CoverStrategyType.valueOf(measurements[5]);
+          nHopReplication = Integer.parseInt(measurements[6]);
+          break;
+        case LOAD_GRAPH_COVER_CREATION_START:
+          processGraphCoverCreationStart(graphCoverStrategy, nHopReplication,
+                  Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_COVER_CREATION_END:
+          if (nHopReplication > 0) {
+            processGraphCoverCreationEnd(graphCoverStrategy, nHopReplication,
+                    Long.parseLong(measurements[4]));
+          }
+          break;
+        case LOAD_GRAPH_NHOP_REPLICATION_END:
+          processGraphCoverCreationEnd(graphCoverStrategy, nHopReplication,
+                  Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_ENCODING_START:
+          processDictionaryEncodingStart(graphCoverStrategy, nHopReplication,
+                  Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_ENCODING_END:
+          processDictionaryEncodingEnd(graphCoverStrategy, nHopReplication,
+                  Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_FILE_TRANSFER_TO_SLAVES_START:
+          processChunkTransferToSlavesStart(graphCoverStrategy, nHopReplication,
+                  Utilities.getComputerId(measurements), Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_FILE_TRANSFER_TO_SLAVES_END:
+          processChunkTransferToSlavesEnd(graphCoverStrategy, nHopReplication,
+                  Utilities.getComputerId(measurements), Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_STORING_TRIPLES_START:
+          processIndexingStart(graphCoverStrategy, nHopReplication,
+                  Utilities.getComputerId(measurements), Long.parseLong(measurements[4]));
+          break;
+        case LOAD_GRAPH_STORING_TRIPLES_END:
+          processIndexingEnd(graphCoverStrategy, nHopReplication,
+                  Utilities.getComputerId(measurements), Long.parseLong(measurements[4]));
+          break;
+        default:
+          // all other types are not required
+          break;
+      }
+    }
+  }
+
+  protected abstract void processGraphCoverCreationStart(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, long startTime);
+
+  protected abstract void processGraphCoverCreationEnd(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, long endTime);
+
+  protected abstract void processDictionaryEncodingStart(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, long startTime);
+
+  protected abstract void processDictionaryEncodingEnd(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, long endTime);
+
+  protected abstract void processChunkTransferToSlavesStart(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, int slave, long startTime);
+
+  protected abstract void processChunkTransferToSlavesEnd(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, int slave, long endTime);
+
+  protected abstract void processIndexingStart(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, int slave, long startTime);
+
+  protected abstract void processIndexingEnd(CoverStrategyType graphCoverStrategy,
+          int nHopReplication, int slave, long endTime);
+
+}
---
+++ src/de/unikoblenz/west/cep/measurementProcessor/utils/Utilities.java
@@ -0,0 +1,29 @@
+package de.unikoblenz.west.cep.measurementProcessor.utils;
+
+import de.uni_koblenz.west.koral.common.measurement.MeasurementType;
+
+/**
+ * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
+ *
+ */
+public class Utilities {
+
+  public static MeasurementType getMeasurementType(String... measurements) {
+    try {
+      return MeasurementType.valueOf(measurements[3]);
+    } catch (IllegalArgumentException e) {
+      // this is an unknown measurement type
+      return null;
+    }
+  }
+
+  public static int getComputerId(String... measurements) {
+    String computerId = measurements[0];
+    if (!computerId.startsWith("slave")) {
+      return 0;
+    } else {
+      return Integer.parseInt(computerId.split("slave[0]*")[1]);
+    }
+  }
+
+}

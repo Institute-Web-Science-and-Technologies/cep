023e56aad016bead3447ae0d45842a3710aff7f5
Daniel Janke <danijank@uni-koblenz.de>
2016-05-13 17:29:47 +0200

    created diagrams for data transfer

---
+++ diagramCreator/dataTransfer/DataTransfer_Covers_at_JoinNumber.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import sys
+import csv
+
+if len(sys.argv) != 4:
+  print("You must have the following arguments: <computationalEffort.csv> <outputDir> <imageType>")
+  sys.exit()
+
+inputFile = sys.argv[1]
+outputDir = sys.argv[2]
+imageType = sys.argv[3]
+
+if not os.path.exists(outputDir):
+  os.makedirs(outputDir)
+
+# for totalComputationalEffort, entropy, standard deviation
+# for each joinnumber create a diagram that
+# shows for each query
+# the computational effort per cover in a separate bar
+
+# required map: joinNumber -> cover -> query -> measurmentType -> value
+
+joinNumbers = {}
+
+with open(inputFile, 'rb') as f:
+  reader = csv.reader(f, delimiter='\t')
+  reader.next()
+  for row in reader:
+    cover = ""
+    if int(row[1]) != 0:
+      cover += row[1] + "HOP_"
+    cover += row[0]
+    joinNumber = row[5]
+    if not joinNumber in joinNumbers:
+      joinNumbers[joinNumber] = {}
+    if not cover in joinNumbers[joinNumber]:
+      joinNumbers[joinNumber][cover] = {}
+    query = ("ss" if row[4]=='SUBJECT_SUBJECT_JOIN' else "so") + " tree=" + ("ll" if row[2]=='LEFT_LINEAR' else ("rl" if row[2]=='RIGHT_LINEAR' else "b")) + " #ds=" + row[6] + " sel=" + row[7]
+    joinNumbers[joinNumber][cover][query] = { "Data Transfer":long(row[8])}
+
+for measurementType in ["Data Transfer"]:
+  for joinNumber in joinNumbers.keys():
+    coverSet = list(sorted(joinNumbers[joinNumber].keys()))
+    dataRows = []
+    queryGroups = []
+    for i, cover in enumerate(coverSet):
+      if i == 0:
+        queryGroups = list(sorted(joinNumbers[joinNumber][cover].keys()))
+      dataRows.append([])
+      for query in queryGroups:
+        dataRows[i].append(joinNumbers[joinNumber][cover][query][measurementType]);
+    # create diagramm
+    n_groups = len(queryGroups)
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 1/float(len(coverSet)+1)
+    rects = []
+    colorBase = 1 / float(len(coverSet)+1)
+    for i, cover in enumerate(coverSet):
+      colorValue = "{:f}".format(colorBase*(i+0.5))
+      rects.append(plt.bar(index + i * bar_width + 0.5*bar_width, np.array(dataRows[i]), bar_width, color=colorValue, label=cover))
+    plt.xlabel("Queries")
+    plt.ylabel(measurementType)
+    plt.title('Data transfer for ' + joinNumber + ' join' + ( "" if int(joinNumber) == 1 else "s"))
+    plt.xticks(index + 0.5, np.array(queryGroups))
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.axis('tight')
+    fig_size = plt.rcParams["figure.figsize"]
+    fig_size[0] = fig_size[0]
+    plt.legend()
+    plt.savefig(outputDir+'/dataTransfer_'+measurementType+'_joinNumber-'+joinNumber+'_forAll_covers.'+imageType, bbox_inches='tight')
+
---
+++ diagramCreator/dataTransfer/DataTransfer_Covers_at_TreeType.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import sys
+import csv
+
+if len(sys.argv) != 4:
+  print("You must have the following arguments: <computationalEffort.csv> <outputDir> <imageType>")
+  sys.exit()
+
+inputFile = sys.argv[1]
+outputDir = sys.argv[2]
+imageType = sys.argv[3]
+
+if not os.path.exists(outputDir):
+  os.makedirs(outputDir)
+
+# for totalComputationalEffort, entropy, standard deviation
+# for each tree type create a diagram that
+# shows for each query
+# the computational effort per cover in a separate bar
+
+# required map: treetype -> cover -> query -> measurmentType -> value
+
+treeTypes = {}
+
+with open(inputFile, 'rb') as f:
+  reader = csv.reader(f, delimiter='\t')
+  reader.next()
+  for row in reader:
+    cover = ""
+    if int(row[1]) != 0:
+      cover += row[1] + "HOP_"
+    cover += row[0]
+    treeType = row[2]
+    if not treeType in treeTypes:
+      treeTypes[treeType] = {}
+    if not cover in treeTypes[treeType]:
+      treeTypes[treeType][cover] = {}
+    query = ("ss" if row[4]=='SUBJECT_SUBJECT_JOIN' else "so") + " #j=" + row[5] + " #ds=" + row[6] + " sel=" + row[7]
+    treeTypes[treeType][cover][query] = { "Data Transfer":long(row[8])}
+
+for measurementType in ["Data Transfer"]:
+  for treeType in treeTypes.keys():
+    coverSet = list(sorted(treeTypes[treeType].keys()))
+    dataRows = []
+    queryGroups = []
+    for i, cover in enumerate(coverSet):
+      if i == 0:
+        queryGroups = list(sorted(treeTypes[treeType][cover].keys()))
+      dataRows.append([])
+      for query in queryGroups:
+        dataRows[i].append(treeTypes[treeType][cover][query][measurementType]);
+    # create diagramm
+    n_groups = len(queryGroups)
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 1/float(len(coverSet)+1)
+    rects = []
+    colorBase = 1 / float(len(coverSet)+1)
+    for i, cover in enumerate(coverSet):
+      colorValue = "{:f}".format(colorBase*(i+0.5))
+      rects.append(plt.bar(index + i * bar_width + 0.5*bar_width, np.array(dataRows[i]), bar_width, color=colorValue, label=cover))
+    plt.xlabel("Queries")
+    plt.ylabel(measurementType)
+    plt.title('Data transfer for query execution tree type ' + treeType)
+    plt.xticks(index + 0.5, np.array(queryGroups))
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.axis('tight')
+    plt.legend()
+    plt.savefig(outputDir+'/dataTransfer_'+measurementType+'_treeType-'+treeType+'_forAll_covers.'+imageType, bbox_inches='tight')
+

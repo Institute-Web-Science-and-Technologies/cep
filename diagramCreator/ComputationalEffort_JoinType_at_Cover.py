c7ae668f0df95ba62005fccabc76cc2d49691854
Daniel Janke <danijank@uni-koblenz.de>
2016-05-13 15:01:33 +0200

    added additional diagram

---
+++ diagramCreator/ComputationalEffort_JoinType_at_Cover.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import sys
+import csv
+
+if len(sys.argv) != 4:
+  print("You must have the following arguments: <computationalEffort.csv> <outputDir> <imageType>")
+  sys.exit()
+
+inputFile = sys.argv[1]
+outputDir = sys.argv[2]
+imageType = sys.argv[3]
+
+if not os.path.exists(outputDir):
+  os.makedirs(outputDir)
+
+# for totalComputationalEffort, entropy, standard deviation
+# for each cover create a diagram that
+# shows for each query
+# the computational effort per tree type in a separate bar
+
+# required map: treetype -> cover -> query -> measurmentType -> value
+
+coverTypes = {}
+
+with open(inputFile, 'rb') as f:
+  reader = csv.reader(f, delimiter='\t')
+  reader.next()
+  for row in reader:
+    cover = ""
+    if int(row[1]) != 0:
+      cover += row[1] + "HOP_"
+    cover += row[0]
+    joinType = row[4]
+    if not cover in coverTypes:
+      coverTypes[cover] = {}
+    if not joinType in coverTypes[cover]:
+      coverTypes[cover][joinType] = {}
+    query = "tree=" + ("ll" if row[2]=='LEFT_LINEAR' else ("rl" if row[2]=='RIGHT_LINEAR' else "b")) + " #j=" + row[5] + " #ds=" + row[6] + " sel=" + row[7]
+    coverTypes[cover][joinType][query] = { "Total":long(row[8]), "Entropy":float(row[9]), "Standard Deviation":float(row[10])}
+
+for measurementType in ["Total", "Entropy", "Standard Deviation"]:
+  for cover in coverTypes.keys():
+    joinTypeSet = list(sorted(coverTypes[cover].keys()))
+    dataRows = []
+    queryGroups = []
+    for i, joinType in enumerate(joinTypeSet):
+      if i == 0:
+        queryGroups = list(sorted(coverTypes[cover][joinType].keys()))
+      dataRows.append([])
+      for query in queryGroups:
+        dataRows[i].append(coverTypes[cover][joinType][query][measurementType]);
+    # create diagramm
+    n_groups = len(queryGroups)
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 1/float(len(joinTypeSet)+1)
+    rects = []
+    colorBase = 1 / float(len(joinTypeSet)+1)
+    for i, joinType in enumerate(joinTypeSet):
+      colorValue = "{:f}".format(colorBase*(i+0.5))
+      rects.append(plt.bar(index + i * bar_width, np.array(dataRows[i]), bar_width, color=colorValue, label=joinType))
+    plt.xlabel("Queries")
+    plt.ylabel(measurementType)
+    plt.title('Computational effort for graph cover ' + cover)
+    plt.xticks(index + bar_width, np.array(queryGroups))
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.legend()
+    plt.savefig(outputDir+'/computationalEffort_'+measurementType+'_cover-'+cover+'_forAll_joinTypes.'+imageType, bbox_inches='tight')
+

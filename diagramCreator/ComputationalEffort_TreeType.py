3e7ee0a5900a0f16f3c7b3c8e3f5a00949813ff1
Daniel Janke <danijank@uni-koblenz.de>
2016-05-13 12:54:23 +0200

    created diagrams for computational effort

---
+++ diagramCreator/ComputationalEffort_TreeType.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import sys
+import csv
+
+if len(sys.argv) != 4:
+  print("You must have the following arguments: <computationalEffort.csv> <outputDir> <imageType>")
+  sys.exit()
+
+inputFile = sys.argv[1]
+outputDir = sys.argv[2]
+imageType = sys.argv[3]
+
+if not os.path.exists(outputDir):
+  os.makedirs(outputDir)
+
+# for totalComputationalEffort, entropy, standard deviation
+# for each tree type create a diagram that
+# shows for each query
+# the computational effort per cover in a separate bar
+
+# required map: treetype -> cover -> query -> measurmentType -> value
+
+treeTypes = {}
+
+with open(inputFile, 'rb') as f:
+  reader = csv.reader(f, delimiter='\t')
+  reader.next()
+  for row in reader:
+    cover = ""
+    if int(row[1]) != 0:
+      cover += row[1] + "HOP_"
+    cover += row[0]
+    treeType = row[2]
+    if not treeType in treeTypes:
+      treeTypes[treeType] = {}
+    if not cover in treeTypes[treeType]:
+      treeTypes[treeType][cover] = {}
+    query = ("ss" if row[4]=='SUBJECT_SUBJECT_JOIN' else "so") + " #j=" + row[5] + " #ds=" + row[6] + " sel=" + row[7]
+    treeTypes[treeType][cover][query] = { "Total":long(row[8]), "Entropy":float(row[9]), "Standard Deviation":float(row[10])}
+
+for measurementType in ["Total", "Entropy", "Standard Deviation"]:
+  for treeType in treeTypes.keys():
+    coverSet = list(sorted(treeTypes[treeType].keys()))
+    dataRows = []
+    queryGroups = []
+    for i, cover in enumerate(coverSet):
+      if i == 0:
+        queryGroups = list(sorted(treeTypes[treeType][cover].keys()))
+      dataRows.append([])
+      for query in queryGroups:
+        dataRows[i].append(treeTypes[treeType][cover][query][measurementType]);
+    # create diagramm
+    n_groups = len(queryGroups)
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    rects = []
+    colorBase = 1 / float(len(coverSet)+2)
+    for i, cover in enumerate(coverSet):
+      colorValue = "{:f}".format(colorBase*(i+1))
+      rects.append(plt.bar(index + i * bar_width, np.array(dataRows[i]), bar_width, color=colorValue, label=cover))
+    plt.xlabel("Queries")
+    plt.ylabel(measurementType)
+    plt.title('Computational effort for query execution tree type ' + treeType)
+    plt.xticks(index + bar_width, np.array(queryGroups))
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.legend()
+    plt.savefig(outputDir+'/computationalEffort_'+measurementType+'_treeType-'+treeType+'.'+imageType, bbox_inches='tight')
+
+#coverNames = np.array(coverNames)
+#coverCreationTimes = np.array(coverCreationTimes)
+#encodingTimes = np.array(encodingTimes)
+#transferTimes = np.array(transferTimes)
+#indexingTimes = np.array(indexingTimes)
+
+sys.exit()
+
+n_groups = 5
+
+means_men = (20, 35, 30, 35, 27)
+std_men = (2, 3, 4, 1, 2)
+
+means_women = (25, 32, 34, 20, 25)
+std_women = (3, 5, 2, 3, 3)
+
+fig, ax = plt.subplots()
+
+index = np.arange(n_groups)
+bar_width = 0.35
+
+opacity = 0.4
+error_config = {'ecolor': '0.3'}
+
+rects1 = plt.bar(index, means_men, bar_width,
+                 alpha=opacity,
+                 color='b',
+                 yerr=std_men,
+                 error_kw=error_config,
+                 label='Men')
+
+rects2 = plt.bar(index + bar_width, means_women, bar_width,
+                 alpha=opacity,
+                 color='r',
+                 yerr=std_women,
+                 error_kw=error_config,
+                 label='Women')
+
+plt.xlabel('Group')
+plt.ylabel('Scores')
+plt.title('Scores by group and gender')
+plt.xticks(index + bar_width, ('A', 'B', 'C', 'D', 'E'))
+plt.legend()
+
+plt.savefig(outputDir+'/computationalEffort_CoverXTreeType-Time.'+imageType, bbox_inches='tight')

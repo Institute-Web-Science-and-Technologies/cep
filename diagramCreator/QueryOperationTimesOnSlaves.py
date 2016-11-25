8a8cc5a7dc6b8eb0c8fb5e866f44014a0a8811ca
Daniel Janke <danijank@uni-koblenz.de>
2016-11-25 15:35:58 +0100

    started creation of diagrams

---
+++ diagramCreator/QueryOperationTimesOnSlaves.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+#
+# This file is part of CEP.
+#
+# CEP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CEP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Leser General Public License
+# along with CEP.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2016 Daniel Janke
+#
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import sys
+import csv
+
+if len(sys.argv) != 4:
+  print("You must have the following arguments: <operationTimesPerSlave.csv> <outputDir> <imageType>")
+  sys.exit()
+
+inputFile = sys.argv[1]
+outputDir = sys.argv[2]
+imageType = sys.argv[3]
+
+if not os.path.exists(outputDir):
+  os.makedirs(outputDir)
+
+# required map: cover -> chunk -> value
+
+queries = {}
+
+with open(inputFile, 'rb') as f:
+  reader = csv.reader(f, delimiter='\t')
+  reader.next()
+  for row in reader:
+    cover = ""
+    if int(row[2]) != 0:
+      cover += row[2] + "HOP_"
+    cover += row[0]
+    scale = row[1]
+    scale = int(row[1]);
+    treeType = row[3]
+    query = ("ss" if row[5]=='SUBJECT_SUBJECT_JOIN' else "so") + " #tp=" + str(int(row[6])+1) + " #ds=" + row[7] + " sel=" + row[8]
+    if not query in queries:
+      queries[query] = {}
+    if not scale in queries[query]:
+      queries[query][scale] = {}
+    if not treeType in queries[query][scale]:
+      queries[query][scale][treeType] = {}
+    if not cover in queries[query][scale][treeType]:
+      queries[query][scale][treeType][cover] = {}
+    for i in range(len(row)):
+      if i >= 9 and (i-9)%3==0 :
+        slave = row[i]
+        operation = row[i+1]
+        time = long(row[i+2])/1000
+        if not operation in queries[query][scale][treeType][cover]:
+          queries[query][scale][treeType][cover][operation] = {}
+        queries[query][scale][treeType][cover][operation][slave] = time
+
+for measurementType in ["Execution Time"]:
+  querySet = list(sorted(queries.keys()))
+  scaleSet = list(sorted(queries[querySet[0]].keys()))
+  treeTypeSet = list(sorted(queries[querySet[0]][scaleSet[0]].keys()))
+  coverSet = list(sorted(queries[querySet[0]][scaleSet[0]][treeTypeSet[0]].keys()))
+  dataRows = {}
+  for i, query in enumerate(querySet):
+    dataRows[query] = {}
+    for j, scale in enumerate(scaleSet):
+      dataRows[query][scale] = {}
+      for k, treeType in enumerate(treeTypeSet):
+        dataRows[query][scale][treeType] = {}
+        for l, cover in enumerate(coverSet):
+          dataRows[query][scale][treeType][cover] = {}
+          for m, operation in enumerate(queries[query][scale][treeType][cover].keys()):
+            dataRows[query][scale][treeType][cover][operation] = []
+            for n, slave in enumerate(queries[query][scale][treeType][cover][operation]):
+                dataRows[query][scale][treeType][cover][operation].append(queries[query][scale][treeType][cover][operation][slave]);
+
+  # create diagramms per treeType
+  for i, query in enumerate(querySet):
+    for j, scale in enumerate(scaleSet):
+      for k, treeType in enumerate(treeTypeSet):
+        for l, cover in enumerate(coverSet):
+          operationSet = queries[query][scale][treeType][cover].keys()
+          slaveSet = list(sorted(queries[query][scale][treeType][cover][list(operationSet)[0]].keys()));
+          n_groups = scale
+          fig, ax = plt.subplots()
+          fig = plt.figure(figsize=(fig.get_figwidth()*(scale/20.),5))
+          index = np.arange(n_groups)
+          bar_width = 1/float(len(operationSet)+1)
+          rects = []
+          colorBase = 1 / float(len(operationSet)+1)
+          for n, operation in enumerate(operationSet):
+            colorValue = "{:f}".format(colorBase*(n+0.5))
+            rects.append(plt.bar(index + n * bar_width + 0.5*bar_width, np.array(dataRows[query][scale][treeType][cover][operation]), bar_width, color=colorValue, label=operation, log=False, bottom=0))
+          plt.xlabel("Slaves")
+          plt.ylabel(measurementType + ' (in sec)')
+          plt.xticks(index + 0.5, np.array(slaveSet))
+          plt.setp(plt.gca().get_xticklabels(), rotation=90, horizontalalignment='right')
+          plt.legend(bbox_to_anchor=(1.03,0,1/(scale/15.),1), loc='upper left', ncol=1, mode="expand", borderaxespad=0.)
+          plt.title('Query operation execution time for query ' + query + '\n' + treeType + ' tree, ' + cover + ' cover and ' + str(scale) + ' chunks', y=1.05)
+          plt.savefig(outputDir+'/queryOperationTime'+'_cover='+cover+'_scale='+str(scale)+'_query='+query+'_treeType='+treeType+'.'+imageType, bbox_inches='tight')
+    sys.exit()
